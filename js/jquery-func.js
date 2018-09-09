404baf27b88b1a5d7e05642fa6765bc4c43f1ecf
kivanzandy <kivan.zandy@gmail.com>
2018-09-09 17:25:18 +0430

    Add files via upload

---
+++ js/jquery-func.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+
+  //=================================== Nav Responsive ===================================//
+    $('#menu').tinyNav({
+       active: 'selected'
+    });
+  
+    
+  //=================================== Subtmit Form  =================================//
+
+	$('#form').submit(function(event) {  
+	  event.preventDefault();  
+	  var url = $(this).attr('action');  
+	  var datos = $(this).serialize();  
+	  $.get(url, datos, function(resultado) {  
+	    $('#result').html(resultado);  
+	  });  
+	}); 
+
+  //=================================== Slide Border  =================================//
+    $('#dg-container').gallery();
+
+ //=================================== Nav Superfish ===============================//
+    jQuery(document).ready(function() {
+		jQuery('ul.sf-menu').superfish();
+	});
+
+         
+});
+
+$('.tool_tip').tooltip('hide')
